2ce26e216962b2cd3265a7f57c2938dacef6dce7
shohelrana2002 <rana2021tang@gmail.com>
2024-10-10 21:04:39 +0600

    jwt add but not verfie

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
-const express = require("express");
 const cors = require("cors");
+const jwt = require("jsonwebtoken");
+const express = require("express");
+const cookieParser = require("cookie-parser");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 const app = express();
+// jwt token require
+
 const port = process.env.PORT || 5000;
 
 // middle were
-app.use(cors());
+app.use(
+  cors({
+    origin: ["http://localhost:5173"],
+    credentials: true,
+  })
+);
 app.use(express.json());
+app.use(cookieParser());
 
 app.get("/", (req, res) => {
   res.send("Cars Doctor is Running now");
@@ -31,6 +41,23 @@ async function run() {
     const serviceCollection = client.db("carDoctor").collection("services");
     const bookingCollection = client.db("carDoctor").collection("bookings");
 
+    //  jwt token
+    app.post("/jwt", (req, res) => {
+      const user = req.body;
+      console.log(user);
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN, {
+        expiresIn: "1h",
+      });
+      res
+        .cookie("token", token, {
+          httpOnly: true,
+          secure: false, // https hole true ha be
+          // sameSite: "none",
+        })
+        .send({ success: true });
+    });
+
+    // services///
     app.get("/services", async (req, res) => {
       const cursor = serviceCollection.find();
       const result = await cursor.toArray();
@@ -57,11 +84,20 @@ async function run() {
     // just jeta login acge emil ota paowar jono summ
 
     app.get("/bookings", async (req, res) => {
-      const result = await bookingCollection.find().toArray();
       let query = {};
+      console.log(req.query.email);
+      // console.log("tok took token", req.cookies.token);
       if (req.query?.email) {
         query = { email: req.query.email };
       }
+      const result = await bookingCollection.find().toArray();
+      res.send(result);
+    });
+
+    app.delete("/bookings/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await bookingCollection.deleteOne(query);
       res.send(result);
     });
 
